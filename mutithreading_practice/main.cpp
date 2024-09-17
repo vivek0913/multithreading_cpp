24f4000033f2320445a6b3987fe31ef601524750
vivekmskn <160849909+vivekmskn@users.noreply.github.com>
2024-09-18 00:08:23 +0530

    Initial Commit

---
+++ mutithreading_practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mutithreading_practice
+//
+//  Created by MacBook on 18/09/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
